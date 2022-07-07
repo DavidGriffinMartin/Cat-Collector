f12aa20562c1eb534c4c13fbd2ab043aedf4ab50
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-07 17:16:02 -0400

    wip: Refactors code.

--- main_app/views.py
+++ main_app/views.py
@@ -52,19 +52,14 @@ def assoc_toy(request, cat_id, toy_id):
 
 
 def add_photo(request, cat_id):
-    # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + \
             photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
             photo = Photo(url=url, cat_id=cat_id)
             photo.save()
         except:
