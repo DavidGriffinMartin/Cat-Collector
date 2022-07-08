35973969b743e46002b072374d1ff50d8d5ced60
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-08 13:46:47 -0400

    wip: Refactors code to show errors upon sign up.

--- main_app/views.py
+++ main_app/views.py
@@ -81,12 +81,18 @@ def signup(request):
     error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid():
+        # if form.is_valid():
+        #     user = form.save()
+        #     login(request, user)
+        #     return redirect('index')
+        # else:
+        #     error_message = 'Invalid sign up - try again'
+        try:
             user = form.save()
             login(request, user)
             return redirect('index')
-        else:
-            error_message = 'Invalid sign up - try again'
+        except:
+            error_message = form.errors
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
