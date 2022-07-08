b199e9d62a5909e75d705691a2cd80f676ceea76
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-07-08 11:37:37 -0400

    wip: Creates sign up form function and URL.

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Cat, Toy, Photo
 from .forms import FeedingForm
 
@@ -65,6 +68,21 @@ def add_photo(request, cat_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', cat_id=cat_id)
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
 class CatCreate(CreateView):
